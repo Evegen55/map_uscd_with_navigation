cab172d9f3925a40dfc180046e7d806bc9b919b1
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-10-20 20:08:19 +0300

    improvement

--- src/main/java/application_with_features/controllers/GmapfxController.java
+++ src/main/java/application_with_features/controllers/GmapfxController.java
@@ -12,8 +12,7 @@ import javafx.scene.layout.AnchorPane;
 /**
  * @author (created on 10/20/2017).
  */
-public class GmapfxController  implements MapComponentInitializedListener {
-
+public class GmapfxController implements MapComponentInitializedListener {
 
     @FXML
     private AnchorPane myPaneWithMapsAndOtherFeatures;
@@ -44,7 +43,10 @@ public class GmapfxController  implements MapComponentInitializedListener {
                 .scaleControl(false)
                 .streetViewControl(false)
                 .zoom(8)
-                .zoomControl(true);
+                .zoomControl(true)
+                .styleString("[{'featureType':'landscape','stylers':[{'saturation':-100},{'lightness':65},{'visibility':'on'}]},{'featureType':'poi','stylers':[{'saturation':-100},{'lightness':51},{'visibility':'simplified'}]},{'featureType':'road.highway','stylers':[{'saturation':-100},{'visibility':'simplified'}]},{\"featureType\":\"road.arterial\",\"stylers\":[{\"saturation\":-100},{\"lightness\":30},{\"visibility\":\"on\"}]},{\"featureType\":\"road.local\",\"stylers\":[{\"saturation\":-100},{\"lightness\":40},{\"visibility\":\"on\"}]},{\"featureType\":\"transit\",\"stylers\":[{\"saturation\":-100},{\"visibility\":\"simplified\"}]},{\"featureType\":\"administrative.province\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"water\",\"elementType\":\"labels\",\"stylers\":[{\"visibility\":\"on\"},{\"lightness\":-25},{\"saturation\":-100}]},{\"featureType\":\"water\",\"elementType\":\"geometry\",\"stylers\":[{\"hue\":\"#ffff00\"},{\"lightness\":-25},{\"saturation\":-97}]}]");
+
+//        ;
 
         map = mapComponent.createMap(options);
     }
